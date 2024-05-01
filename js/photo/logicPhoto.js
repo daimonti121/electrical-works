8f4e8b7a6cdc4e835a9c7a52ab59074f0e8c703b
Yushenko Dima <damios.1812@gmail.com>
2024-05-01 17:18:37 +0200

    "add arrows for slick galery"

--- js/photo/logicPhoto.js
+++ js/photo/logicPhoto.js
@@ -1,32 +1,9 @@
-// $('.image-box').magnificPopup({
-//     delegate: 'a',
-//     type: 'image',
-//     closeOnContentClick: true,
-//     closeBtnInside: true,
-//     galerry: {
-//         enabled: true
-//     }
-// });
-
-
-// import PhotoSwipeLightbox from '../photo/photoswipe-lightbox.esm.js';
-// import PhotoSwipeLightbox from '../photo/photoswipe.esm.js';
-
-// const lightbox = new PhotoSwipeLightbox({
-//     gallery: '#my-gallery',
-//     children: 'a',
-//     pswpModule: () => import('https://unpkg.com/photoswipe'),
-// });
-
-// lightbox.init();
-
 
 import PhotoSwipeLightbox from '../photo/photoswipe-lightbox.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
     // may select multiple "galleries"
     gallery: '#my-gallery',
-    pinchToClose: false,
     showHideAnimationType: 'none',
 
     // Elements within gallery (slides)
--- slick/logic.js
+++ slick/logic.js
@@ -2,7 +2,7 @@ $('.center').slick({
   centerMode: true,
   centerPadding: '20px',
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 2000,
   slidesToShow: 3,
   dots: false,
   responsive: [
@@ -42,10 +42,61 @@ $('.center').slick({
         arrows: false,
         centerMode: true,
         centerPadding: '0px',
-        autoplay: false, 
+        autoplay: true, 
         slidesToShow: 1, 
         dots: true
       }
     }
+  ]
+});
+
+$('.second').slick({
+  centerMode: true,
+  centerPadding: '20px',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 3,
+  dots: false,
+  responsive: [
+      {
+          breakpoint: 1000,
+          settings: {
+              centerMode: true,
+              centerPadding: '0px',
+              slidesToShow: 2,
+          }
+      },
+      {
+          breakpoint: 979,
+          settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '0px',
+              autoplay: true,
+              slidesToShow: 3,
+              dots: true
+          }
+      },
+      {
+          breakpoint: 760,
+          settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '0px',
+              autoplay: true,
+              slidesToShow: 2,
+              dots: true
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              arrows: true,
+              centerMode: true,
+              centerPadding: '0px',
+              autoplay: true,
+              slidesToShow: 1,
+          }
+      }
   ]
 });
