72cf196a82bccf459be448ac18880aedce926b81
Yushenko Dima <damios.1812@gmail.com>
2024-05-15 08:52:08 +0200

    'add active navigation'

---
+++ js/scroll/scroll.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        /*если прокрутка больше 300 показ. кнопку*/
+        if ($(this).scrollTop() > 1500) {
+            $('.gototop, .alert').fadeIn();
+            /*если нет то скрываем кнопку*/
+        } else {
+            $('.gototop, .alert').fadeOut();
+        }
+    });
+    /*при клике по кнопке переходим вверх стр.*/
+    $('.gototop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
+});     
